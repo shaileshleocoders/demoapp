b4aaa4360e24b1b44c65feac743dc468413be872
shaileshleocoders <118794078+shaileshleocoders@users.noreply.github.com>
2023-12-21 15:24:28 +0530

    user table

--- src/components/modules/users/User.jsx
+++ src/components/modules/users/User.jsx
@@ -1,20 +1,39 @@
 import React, { useState } from 'react'
+import { users } from '../../../user';
 
 const User = () => {
     const [name, setname] = useState("");
     const [surname, setsurname] = useState("");
     const [count, setcount] = useState(0);
     const [value, setvalue] = useState(null);
+    const [userData, setuserData] = useState(users);
 
     const changeName = () => {
         setname("tejas")
         setsurname("lilhare")
     }
+
+    const handleDelete = (id, index) => {
+        let arr = [...userData];
+        const newarr = arr.filter((user) => user.id !== id)
+        console.log(newarr)
+        setuserData(newarr);
+
+        // arr.splice(index, 1);
+        // setuserData(arr);
+
+        // const obj = arr.find((user) => user.id == id);
+        // const i = arr.indexOf(obj)
+        // console.log(i)
+        // arr.splice(i, 1);
+        // setuserData(arr);
+    }
+
     return (
         <div>
             {/* <h1>{`${name} ${surname}`}</h1>
             <button onClick={changeName}>change Name</button> */}
-            <input type="number" onChange={(e) => setvalue(+e.target.value)} value={value} />
+            {/* <input type="number" onChange={(e) => setvalue(+e.target.value)} value={value} />
             <div className="btn btn-sm btn-primary" onClick={() => {
                 setcount(0)
                 setvalue(0)
@@ -24,7 +43,37 @@ const User = () => {
                 <div className="btn btn-sm btn-primary" onClick={() => setcount(count + value)}>+</div>
                 {count}
                 <div className="btn btn-sm btn-danger" onClick={() => setcount(count - value)}>-</div>
-            </center>
+            </center> */}
+            {userData.length > 0 ?
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">User Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Age</th>
+                            <th scope="col">Country</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {userData.map((user, index) => {
+                            return (
+                                <tr key={user.id}>
+                                    <th scope="row">{user.id}</th>
+                                    <td>{user.username}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.age}</td>
+                                    <td>{user.country}</td>
+                                    <td><button className='btn btn-sm btn-danger' onClick={() => handleDelete(user.id, index)}>Delete</button></td>
+                                </tr>
+                            )
+                        })
+                        }
+                    </tbody>
+                </table> :
+                <h1 className='text-center'>There is no data found</h1>
+            }
         </div>
     )
 }
---
+++ src/user.js
@@ -0,0 +1,14 @@
+export const users = [
+    { id: 1, username: 'john_doe', email: 'john.doe@example.com', age: 28, country: 'USA' },
+    { id: 2, username: 'alice_smith', email: 'alice.smith@example.com', age: 24, country: 'Canada' },
+    { id: 3, username: 'bob_jackson', email: 'bob.jackson@example.com', age: 35, country: 'UK' },
+    { id: 4, username: 'emily_wilson', email: 'emily.wilson@example.com', age: 31, country: 'Australia' },
+    { id: 5, username: 'michael_jones', email: 'michael.jones@example.com', age: 29, country: 'Germany' },
+    { id: 6, username: 'sara_brown', email: 'sara.brown@example.com', age: 26, country: 'France' },
+    { id: 7, username: 'peter_evans', email: 'peter.evans@example.com', age: 32, country: 'Spain' },
+    { id: 8, username: 'lisa_miller', email: 'lisa.miller@example.com', age: 27, country: 'Italy' },
+    { id: 9, username: 'kevin_taylor', email: 'kevin.taylor@example.com', age: 30, country: 'Japan' },
+    { id: 10, username: 'olivia_martin', email: 'olivia.martin@example.com', age: 33, country: 'Brazil' },
+  ];
+  
+  
